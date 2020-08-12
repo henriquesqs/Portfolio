21389bbdf218332eabddae515bc3f788a2ffd5b3
henriq <henriquesqs@hotmail.com>
2020-08-11 22:42:00 -0300

    Created Index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./assets/styles/global.css";
+import Index from "./pages/Index";
+
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello, world! :)</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Index />
     );
 }
 
---
+++ src/pages/Index/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./styles.css";
+import Navbar from "../../components/Navbar";
+import ProfilePhoto from "../../assets/images/ProfilePhoto.jpg"
+
+const Index = () => {
+    return (
+        <div className="index">
+
+            {/* Creating my navbar and given its items/links by its props */}
+            <Navbar link1="HOME" link2="SKILLS" link3="PROJECTS" link4="CONTACT"></Navbar>
+
+            <p className="main-text"> Hello! I am </p>
+
+            <p className="authorName">HENRIQUE</p>
+
+            <p className="sub-text">
+                a computer science student and lover of web development, web design and lofi music.
+            </p>
+
+            <img className="ProfilePhoto" src={ProfilePhoto} alt="Profile" />
+
+            
+
+        </div>
+    )
+}
+
+export default Index;
