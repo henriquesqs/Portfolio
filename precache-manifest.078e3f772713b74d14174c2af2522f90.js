8681317f12e86f6eea7a7c15c68d10ab7a747fd6
henriquesqs <36971021+henriquesqs@users.noreply.github.com>
2020-08-12 18:12:19 +0000

    Deploying to gh-pages from  @ 7eca1cf46baea10710a17f969cdc3fcb9e3ff282 🚀

---
+++ precache-manifest.078e3f772713b74d14174c2af2522f90.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e1145de5123898ffe264b909775250",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aaa058ed3d2965535801",
+    "url": "/portfolio/static/css/main.664ae7f7.chunk.css"
+  },
+  {
+    "revision": "94de14090091ff085396",
+    "url": "/portfolio/static/js/2.e1f50d64.chunk.js"
+  },
+  {
+    "revision": "7918f9b893310890f0a444ed75c67550",
+    "url": "/portfolio/static/js/2.e1f50d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa058ed3d2965535801",
+    "url": "/portfolio/static/js/main.342a433c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c2aff2f99cd0ebf25fd01b8e99a10725",
+    "url": "/portfolio/static/media/ProfilePhoto.c2aff2f9.jpg"
+  },
+  {
+    "revision": "83f0d252fafe2f4b699a1ca686a33e4a",
+    "url": "/portfolio/static/media/downloadButton.83f0d252.svg"
+  },
+  {
+    "revision": "c06f947cac49f476e4ccbd2c3ff2e6fc",
+    "url": "/portfolio/static/media/downloadButtonBlue.c06f947c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.21af5bbdd8607bdec29c4d5979dfc494.js"
+  "/portfolio/precache-manifest.078e3f772713b74d14174c2af2522f90.js"
 );
 
 self.addEventListener('message', (event) => {
