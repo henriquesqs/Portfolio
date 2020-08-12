0e49a67e07bdc1e35abc3624193a13d47cad4c3a
henriq <henriquesqs@hotmail.com>
2020-08-12 12:46:50 -0300

    Added React-Fullpage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import "./assets/styles/global.css";
 import Index from "./pages/Index";
-
+import ReactFullpage from '@fullpage/react-fullpage'; // will return static version on server and "live" version on client
+import "fullpage.js/vendors/scrolloverflow";
 
 function App() {
     return (
-        <Index />
+        <ReactFullpage
+
+            //fullpage options
+            licenseKey={'YOUR_KEY_HERE'}
+            scrollingSpeed={1000} /* Options here */
+
+            render={({ state, fullpageApi }) => {
+                return (
+                    <ReactFullpage.Wrapper>
+                        <div className="section">
+                            <Index />
+                        </div>
+                        <div className="section">
+                            <Index />
+                        </div>
+                    </ReactFullpage.Wrapper>
+                );
+            }}
+        />
     );
 }
 
