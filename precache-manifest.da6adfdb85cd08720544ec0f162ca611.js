41682a1c881dca69f24016346f400a14496aa5b0
henriquesqs <36971021+henriquesqs@users.noreply.github.com>
2020-08-13 18:52:51 +0000

    Deploying to gh-pages from  @ 2630e39a33e2738ef4debc66784c8931fafe93cf 🚀

---
+++ precache-manifest.da6adfdb85cd08720544ec0f162ca611.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4987421dbdced80f397a1ed7da17b0aa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "faa6e3f538ae69528098",
+    "url": "/portfolio/static/css/main.c6182cb3.chunk.css"
+  },
+  {
+    "revision": "06bef7d5ee728304023a",
+    "url": "/portfolio/static/js/2.6d6b2d77.chunk.js"
+  },
+  {
+    "revision": "e95b3ebaec68caad50c330e63af5be13",
+    "url": "/portfolio/static/js/2.6d6b2d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa6e3f538ae69528098",
+    "url": "/portfolio/static/js/main.cf73021e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c2aff2f99cd0ebf25fd01b8e99a10725",
+    "url": "/portfolio/static/media/ProfilePhoto.c2aff2f9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4af86195679475c60a5709b0f983e5e8.js"
+  "/portfolio/precache-manifest.da6adfdb85cd08720544ec0f162ca611.js"
 );
 
 self.addEventListener('message', (event) => {
