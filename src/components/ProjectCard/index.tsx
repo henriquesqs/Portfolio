ea7d4ab4bb651d0fda3a27553baf3a97678bb49a
henriquesqs <henriquesqs@hotmail.com>
2020-09-07 17:05:09 -0300

    finished projectCard display

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,35 +1,53 @@
 import React from "react"
+import { Button } from "../Button";
 
 import "./styles.css"
 
 export interface ProjectCardProps {
     title: string;
-    description: string;
+    smallDescription: string;
+    longDescription: string;
     image: string;
     focused: boolean;
+    githubLink?: string;
+    liveLink?: string;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = (props) => {
 
     return (
-        <div>
+        <div id="card">
             {props.focused ?
-
-                <div className="focus_projectCard">
+                <div className="focus_projectCard" aria-disabled="false">
                     <img src={props.image} alt="Project preview" />
+                    <div className="texts">
+                        <p className="projectTitle">{props.title}</p>
+                        <p className="projectDescription">{props.longDescription}</p>
+                    </div>
+                    <div className="cardBottom">
+                        <hr className="textsDivider" />
+                        <div className="buttonsArea">
+                            {props.githubLink ? <Button bgColor="white" textColor="black" borderColor="black" hoverTextColor="#3672A6" hoverBorderColor="#3672A6" btnWidth="45%" btnHeight="50px">GITHUB</Button> : null}
+
+                            {props.liveLink ? <Button bgColor="white" textColor="black" borderColor="black" hoverTextColor="#3672A6" hoverBorderColor="#3672A6" btnWidth="45%" btnHeight="50px">LIVE DEMO</Button> : null}
+                        </div>
+                    </div>
+
                 </div>
                 :
                 <div className="hover">
                     <div className="projectCard">
                         <div className="blackLayer">
-                            <p className="projectTitle">{props.title}</p>
-                            <p className="projectDescription">{props.description}</p>
+                            <div className="texts">
+                                <p className="projectTitle">{props.title}</p>
+                                <p className="projectDescription">{props.smallDescription}</p>
+                            </div>
                         </div>
                         <img src={props.image} alt="Project preview" />
                     </div>
                 </div>
             }
-        </div>
+        </div >
     )
 }
 
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, SyntheticEvent } from "react";
 import proffyTemplate from "../../assets/images/proffy.jpg"
 import ProjectCard from "../../components/ProjectCard";
 
@@ -12,33 +12,53 @@ const Projects = () => {
     const [isThirdCardFocused, setIsThirdCardFocused] = useState(false);
 
     // This method is responsible for handling click on ProjectCards.
-    // When user clicks on a card, we increase this card size and hide the others
-    function handleProjectCardClick(id: string) {
+    // When user clicks on a card, we increase this card size and hide the others.
+    function handleProjectCardClick(id: string, event: SyntheticEvent) {
 
-        if (id === "1") {
-            setIsFirstCardFocused(!isFirstCardFocused)
-        }
-        else if (id === "2") {
-            setIsSecondCardFocused(!isSecondCardFocused)
-        }
-        else {
-            setIsThirdCardFocused(!isThirdCardFocused)
-        }
+        if (id === "1")
+            setIsFirstCardFocused(true)
+
+        else if (id === "2")
+            setIsSecondCardFocused(true)
+
+        else
+            setIsThirdCardFocused(true)
+
+        event.stopPropagation()
+    }
+
+    function handleClickOutsideCard(event: SyntheticEvent) {
+        setIsFirstCardFocused(false)
+        setIsSecondCardFocused(false)
+        setIsThirdCardFocused(false)
     }
 
     return (
-        <div className="projects">
+        <div className="projects" onClick={handleClickOutsideCard}>
             <h1 className="pageTitle">PROJECTS</h1>
             <h3 className="pageSubTitle">Click on any card and check them</h3>
 
-            <div onClick={() => handleProjectCardClick("1")}>
-                {(isSecondCardFocused || isThirdCardFocused) ? null : <ProjectCard focused={isFirstCardFocused} title="TÍTULO" description="1erjliferjkfrlkrl1erjliferjkfrlkrl1erjliferjkfrlkrl1erjliferjkfrlkrl" image={proffyTemplate} />}
-            </div>
-            <div onClick={() => handleProjectCardClick("2")}>
-                {(isFirstCardFocused || isThirdCardFocused) ? null : <ProjectCard focused={isSecondCardFocused} title="TÍTULO" description="2erjliferjkfrlkrl" image={proffyTemplate} />}
-            </div>
-            <div onClick={() => handleProjectCardClick("3")}>
-                {(isFirstCardFocused || isSecondCardFocused) ? null : <ProjectCard focused={isThirdCardFocused} title="TÍTULO" description="3erjliferjkfrlkrl" image={proffyTemplate} />}
+            <div className="buttons">
+                {/* First project card */}
+                {(isSecondCardFocused || isThirdCardFocused) ? null :
+                    <button id="firstCard" onClick={(event) => { handleProjectCardClick("1", event) }}>
+                        <ProjectCard focused={isFirstCardFocused} title="TÍTULO" longDescription="frefrefrferferfer" smallDescription="1erjliferjkfrlkrl1erjliferjkfrlkrl1erjliferjkfrlkrl1erjliferjkfrlkrl" image={proffyTemplate} githubLink="https://github.com" liveLink="#" />
+                    </button>
+                }
+
+                {/* Second project card */}
+                {(isFirstCardFocused || isThirdCardFocused) ? null :
+                    <button id="secondCard" onClick={(event) => { handleProjectCardClick("2", event) }}>
+                        <ProjectCard focused={isSecondCardFocused} title="TÍTULO" longDescription="frefrefrferferfer" smallDescription="2erjliferjkfrlkrl" image={proffyTemplate} githubLink="#" />
+                    </button>
+                }
+
+                {/* Third project card */}
+                {(isFirstCardFocused || isSecondCardFocused) ? null :
+                    <button id="thirdCard" onClick={(event) => { handleProjectCardClick("3", event) }}>
+                        <ProjectCard focused={isThirdCardFocused} title="TÍTULO" longDescription="frefrefrferferfer" smallDescription="3erjliferjkfrlkrl" image={proffyTemplate} />
+                    </button>
+                }
             </div>
 
         </div >
