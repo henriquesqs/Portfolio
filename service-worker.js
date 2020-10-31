67ba0795c97dab3487626c5b5af909e19befb4fc
henriquesqs <36971021+henriquesqs@users.noreply.github.com>
2020-10-31 21:52:55 +0000

    Deploying to gh-pages from  @ 2865896199429516545f8fbda4f2d9263c80a845 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f405a5e69daa5145f71dbc34a33c06ef.js"
+  "/portfolio/precache-manifest.d95f279be756e5bb121eef2fa25af3f1.js"
 );
 
 self.addEventListener('message', (event) => {
