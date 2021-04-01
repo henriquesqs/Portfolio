a91d36a044c90e39a4fadc4e1b3ca7222a2a53e6
henriquesqs <36971021+henriquesqs@users.noreply.github.com>
2021-04-01 12:39:41 +0000

    Deploying to gh-pages from @ henriquesqs/portfolio@593790caa2aac2fee393c7a58934e9e6e321bd17 🚀

---
+++ precache-manifest.f2edff5c580b495318bc5e6d9eb4accc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7887c76d828f9ad59752dd20c0ee3ec2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a403159ea71f1995c419",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a28223a5907134f20bcb",
+    "url": "/portfolio/static/css/main.3b5a4a18.chunk.css"
+  },
+  {
+    "revision": "a403159ea71f1995c419",
+    "url": "/portfolio/static/js/2.b420abbf.chunk.js"
+  },
+  {
+    "revision": "2843005cfe63620d3a1e752107ef83ca",
+    "url": "/portfolio/static/js/2.b420abbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28223a5907134f20bcb",
+    "url": "/portfolio/static/js/main.b4242fb1.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c2aff2f99cd0ebf25fd01b8e99a10725",
+    "url": "/portfolio/static/media/ProfilePhoto.c2aff2f9.jpg"
+  },
+  {
+    "revision": "d693dd88e08b66b0a5be8e4532874638",
+    "url": "/portfolio/static/media/proffy.d693dd88.jpg"
+  }
+]);
